0d9c1275d2046e5a4134a9ed8e991dd999545ee9
unknown <leadingyu88@gmail.com>
2019-03-02 15:09:33 -0800

    358

--- summary/class5/358.java
+++ summary/class5/358.java
@@ -8,13 +8,40 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
         	charFreq[s.charAt(i)]++;
         }
-        PriorityQueue<Integer> indexMapHeap = new PriorityQueue<>((o1, o2) -> Integer.compare(charFreq[o2], charFreq[o1]) == 0 ? Integer.compare(o1, o2) : Integer.compare(charFreq[o2], charFreq[o1]));
+        PriorityQueue<Integer> indexMaxHeap = new PriorityQueue<>((o1, o2) -> Integer.compare(charFreq[o2], charFreq[o1]) == 0 ? Integer.compare(o2, o1) : Integer.compare(charFreq[o2], charFreq[o1]));
         for (int i = 0; i < size; i++) {
         	if (charFreq[i] > 0) {
-        		indexMapHeap.add(i);
+        		indexMaxHeap.add(i);
         	}
         }
-
-        
+        StringBuilder sb = new StringBuilder();
+        while (!indexMaxHeap.isEmpty()) {
+        	final int heapSize = indexMaxHeap.size();
+        	if (heapSize < k) {
+        		for (int i = 0; i < heapSize; i++) {
+        			int index = indexMaxHeap.poll();
+        			charFreq[index]--;
+        			if (charFreq[index] > 0) {
+        				return "";
+        			}
+        			sb.append((char)index );
+        		} 
+        	} else {
+        		ArrayList<Integer> indexTopKList = new ArrayList<>();
+        		for (int i = 0; i < k; i++) {
+        			int index = indexMaxHeap.poll();
+        			indexTopKList.add(index);
+        			charFreq[index]--;
+        			sb.append((char)index );
+        		}
+        		for (int i = 0; i < k; i++) {
+        			int index = indexTopKList.get(i);
+        			if (charFreq[index] > 0) {
+        				indexMaxHeap.add(index);
+        			}
+        		}
+        	}
+        }
+        return sb.toString();
     }
 }
