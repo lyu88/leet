17aac142dbf2c118f9ed62890862127788a99f70
unknown <leadingyu88@gmail.com>
2019-04-06 22:05:17 -0700

    663

--- summary/class19/663.java
+++ summary/class19/663.java
@@ -1,8 +1,8 @@
 class Solution {
     public boolean checkEqualTree(TreeNode root) {
-        int sum = sum(root);
+    	int sum = sum(root);
         if (sum % 2 != 0) {
-            return false;
+        	return false;
         }
         int tgt = sum / 2;
         return dfs(root, tgt).found;
@@ -13,29 +13,29 @@ class Solution {
     }
 
     private Result dfs(TreeNode root, int tgt) {
-        // base case
-        if (root == null) {
-            return new Result(0, false);
-        }
-        // general case
-        Result left = dfs(root.left, tgt);
-        Result right = dfs(root.right, tgt);
+    	// base case
+    	if (root == null) {
+    		return new Result(0, false);
+    	}
+    	// general case
+    	Result left = dfs(root.left, tgt);
+    	Result right = dfs(root.right, tgt);
 
-        boolean found = left.found 
-                        || right.found 
-                        || (root.left != null && left.sum == tgt)
-                        || (root.right != null &&right.sum == tgt);
-        int sum = root.val + left.sum + right.sum;
-        return new Result(sum, found);
+    	boolean found = left.found 
+    					|| right.found 
+    					|| (root.left != null && left.sum == tgt)
+    					|| (root.right != null &&right.sum == tgt);
+    	int sum = root.val + left.sum + right.sum;
+    	return new Result(sum, found);
     }
 
     private class Result {
-        int sum;
-        boolean found;
-        public Result(int sum, boolean found) {
-            this.sum = sum;
-            this.found = found;
-        }
+    	int sum;
+    	boolean found;
+    	public Result(int sum, boolean found) {
+    		this.sum = sum;
+    		this.found = found;
+    	}
     }
 
 }
