f973238ffbb1c16dc1dcd23aa22f887033472793
unknown <saberyu778@gmail.com>
2019-05-02 23:52:41 -0700

    873

--- summary/class30-graph2/873.java
+++ summary/class30-graph2/873.java
@@ -1,5 +1,24 @@
 class Solution {
     public int lenLongestFibSubseq(int[] A) {
+    	final int n = A.length;
+    	int[][] allResults = new int[n][n];
+    	int max = 0;
+    	for (int i = 2; i < n; i++) {
+    		int left = 0, right = i - 1;
+    		while (left < right) {
+	    		if (A[left] + A[right] == A[i]) {
+	    			allResults[right][i] = allResults[left][right] + 1;
+	    			max = Math.max(max, allResults[right][i]);
+	    			left++;
+	    			right--;
+	    		} else if (A[left] + A[right] < A[i]) {
+	    			left++;
+	    		} else {
+	    			right--;
+	    		}
+	    	}
+    	}
     	
+    	return max == 0 ? 0 : max + 2;
     }
 }
