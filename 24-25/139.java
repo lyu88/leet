8c0ab7215d0beaf33c13867d62711e6de98e1bb3
Richard Yu <leadingyu88@Richards-MBP.hsd1.ca.comcast.net>
2025-03-22 21:57:09 -0700

    139 did again on 0322

--- 24-25/139.java
+++ 24-25/139.java
@@ -43,3 +43,22 @@ class Solution {
         return dp[0];
     }
 }
+
+// did again
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        final int len = s.length();
+        boolean[] dp = new boolean[len + 1];
+        dp[len] = true;
+        for (int i = len - 1; i >= 0; i--) {
+            for (int j = i + 1; j <= len; j++) {
+                if (set.contains(s.substring(i,j)) && dp[j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+}
