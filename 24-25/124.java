b455c06d04a6280856e1212956b30c99a1fb4c5e
leadingyu88 <leadingyu88@gmail.com>
2025-04-13 14:02:24 -0700

    124 easier version after doing 543

--- 24-25/124.java
+++ 24-25/124.java
@@ -26,3 +26,41 @@ class Solution {
 
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxPathSum(TreeNode root) {
+        int[] ret = {Integer.MIN_VALUE};
+        dfs(root, ret);
+        return ret[0];
+    }
+
+    private int dfs(TreeNode root, int[] ret) {
+        if (root == null) {
+            return 0;
+        }
+        int left = dfs(root.left, ret);
+        int right = dfs(root.right, ret);
+        int maxWithNode = root.val + Math.max(left, right);
+        if (maxWithNode < root.val) {
+            maxWithNode = root.val;
+        }
+        int max = Math.max(maxWithNode, root.val + left + right);
+        ret[0] = Math.max(ret[0], max);
+        return maxWithNode;
+    }
+}
