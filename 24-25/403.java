1fea8c9d07254646db047c84380011fec0a9f6c1
leadingyu88 <leadingyu88@gmail.com>
2025-04-30 22:29:03 -0700

    403 dp

--- 24-25/403.java
+++ 24-25/403.java
@@ -30,3 +30,64 @@ class Solution {
         return flag;
     }
 }
+
+// time exceeds limit
+class Solution {
+    public boolean canCross(int[] stones) {
+        Map<Integer, Set<Integer>> allRes = new HashMap<>();
+        allRes.put(stones[0], new HashSet<>(Set.of(0)));
+        for (int i = 1; i < stones.length; i++) {
+            int key = stones[i];
+            Set<Integer> res = new HashSet<>();
+            for (int index = 0; index < i; index++) {
+                int distance = stones[i] - stones[index];
+                Set<Integer> set = outSteps(allRes.get(stones[index]));
+                if (set.contains(distance)) {
+                    res.add(distance);
+                }
+            }
+            allRes.put(stones[i], res);
+        }
+        return !allRes.get(stones[stones.length - 1]).isEmpty();
+    }
+
+    private Set<Integer> outSteps(Set<Integer> inSet) {
+        Set<Integer> out = new HashSet<>();
+        for (int k : inSet) {
+            if (k - 1 > 0) {
+                out.add(k-1);
+            }
+            if (k > 0) {
+                out.add(k);
+            }
+            out.add(k+1);
+        }
+        return out;
+    }
+}
+
+//good now
+class Solution {
+    public boolean canCross(int[] stones) {
+        final int len = stones.length;
+        boolean[][] dp = new boolean[len][len];
+        dp[0][1] = true;
+        for (int i = 1; i < len; i++) {
+            for (int index = 0; index < i; index++) {
+                int diff = stones[i] - stones[index];
+                if (diff >= len || !dp[index][diff]) {
+                    continue;
+                }
+                if (i == len - 1) {
+                    return true;
+                }
+                dp[i][diff + 1] = true;
+                dp[i][diff] = true;
+                if (diff - 1 > 0) {
+                    dp[i][diff - 1] = true;
+                }
+            }
+        }
+        return false;
+    }
+}
