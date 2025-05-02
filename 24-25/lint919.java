1995fdc169bf9b88b9e54a06582996766cbb5690
leadingyu88 <leadingyu88@gmail.com>
2025-05-02 11:56:51 -0700

    lint 919 · Meeting Rooms II

---
+++ 24-25/lint919.java
@@ -0,0 +1,38 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param intervals: an array of meeting time intervals
+     * @return: the minimum number of conference rooms required
+     */
+    public int minMeetingRooms(List<Interval> intervals) {
+        // Write your code here
+        intervals.sort((o1, o2) -> Integer.compare(o1.start, o2.start));
+        PriorityQueue<Interval> minHeap = new PriorityQueue<Interval>(
+            (o1, o2) -> Integer.compare(o1.end, o2.end)
+        );
+        for (Interval item : intervals) {
+            if (minHeap.isEmpty()) {
+                minHeap.add(item);
+            } else {
+                if (item.start >= minHeap.peek().end) {
+                    Interval min = minHeap.poll();
+                    min.end = item.end;
+                    minHeap.add(min);
+                } else {
+                    minHeap.add(item);
+                }
+            }
+        }
+        return minHeap.size();
+    }
+}
