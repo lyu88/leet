2bc9cbe69abe8fe2bcfd05b4a412892fb71aa5a5
leadingyu88 <leadingyu88@gmail.com>
2025-05-05 21:59:33 -0700

    Lint 69 · Binary Tree Level Order Traversal

---
+++ 24-25/lint69.java
@@ -0,0 +1,43 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Level order a list of lists of integer
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        // write your code here
+        List<List<Integer>> ret = new ArrayList<>();
+        if (root == null) {
+            return ret;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            final int len = queue.size();
+            List<Integer> res = new ArrayList<>();
+            for (int i = 0; i < len; i++) {
+                TreeNode node = queue.poll();
+                res.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            ret.add(res);
+        }
+        return ret;
+    }
+}
