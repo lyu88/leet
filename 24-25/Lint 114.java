45d8b8f26219af013fef0e4b07f01c4ced764b6e
leadingyu88 <leadingyu88@gmail.com>
2025-05-09 22:59:49 -0700

    Lint 114: unique paths

---
+++ 24-25/Lint 114.java
@@ -0,0 +1,18 @@
+public class Solution {
+    /**
+     * @param m: positive integer (1 <= m <= 100)
+     * @param n: positive integer (1 <= n <= 100)
+     * @return: An integer
+     */
+    public int uniquePaths(int m, int n) {
+        // write your code here
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+}
