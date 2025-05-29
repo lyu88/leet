eb2c392fe1d8aaa1a2bd790c29e158976a05f698
leadingyu88 <leadingyu88@gmail.com>
2025-05-29 07:45:16 -0700

    394. Decode String

---
+++ 24-25/394.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String decodeString(String s) {
+        Deque<Character> queue = new LinkedList<>();
+        for (char c : s.toCharArray()) queue.offer(c);
+        return dfs(queue);
+    }
+
+    private String dfs(Queue<Character> queue) {
+        int num = 0;
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            char c = queue.poll();
+            if (Character.isDigit(c)) {
+                num = num * 10 + c - '0';
+            } else if (c == '[') {
+                String sub = dfs(queue);
+                for (int i = 0; i < num; i++) {
+                    sb.append(sub);
+                }
+                num = 0;
+            } else if (c == ']') {
+                break;
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
