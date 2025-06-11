c459ee217a7f03bbb3feda633f29f24db1c6dcfc
leadingyu88 <leadingyu88@gmail.com>
2025-06-10 21:22:13 -0700

    Lint 641 · Missing Ranges

---
+++ 24-25/Lint641.java
@@ -0,0 +1,29 @@
+public class Solution {
+    /**
+     * @param nums: a sorted integer array
+     * @param lower: An integer
+     * @param upper: An integer
+     * @return: a list of its missing ranges
+     */
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        // write your code here
+        List<String> ret = new ArrayList<>();
+        for (int num : nums) {
+            if (num == lower + 1) {
+                ret.add("" + lower);
+            } else if (num > lower + 1) {
+                ret.add(lower + "->" + String.valueOf(num - 1));
+            }
+            lower = num + 1;
+            if (lower == Integer.MIN_VALUE) {
+                return ret;
+            }
+        }
+        if (lower == upper) {
+            ret.add("" + lower);
+        } else if (lower < upper) {
+            ret.add(lower + "->" + String.valueOf(upper));
+        }
+        return ret;
+    }
+}
