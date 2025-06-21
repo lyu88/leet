39c202588b507d50eea09131b1561506e9c24c75
leadingyu88 <leadingyu88@gmail.com>
2025-06-21 09:42:15 -0700

    69 sqrt(x)

--- 24-25/69.java
+++ 24-25/69.java
@@ -17,3 +17,26 @@ class Solution {
         return end;
     }
 }
+
+
+// no need long but convert to division
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        int start = 1, end = x - 1;
+        while (start < end) {
+            int res = start + (end - start)/2;
+            if (res <= x/res && (res+1) > x/(res+1)) {
+                return res;
+            }
+            if (res > x/res) {
+                end = res - 1;
+            } else {
+                start = res + 1;
+            }
+        }
+        return start;
+    }
+}
