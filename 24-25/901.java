edd90bce36cb1e70426110d8601bb6f003d0ba6d
leadingyu88 <leadingyu88@gmail.com>
2025-07-02 20:26:03 -0700

    764. Largest Plus Sign

--- 24-25/901.java
+++ 24-25/901.java
@@ -21,3 +21,36 @@ class StockSpanner {
  * StockSpanner obj = new StockSpanner();
  * int param_1 = obj.next(price);
  */
+
+// pay attention to the reason why we can use the mono stack
+class StockSpanner {
+
+    Stack<Node> stack;
+
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+    
+    public int next(int price) {
+        if (stack.isEmpty() || stack.peek().val > price) {
+            stack.push(new Node(price, 1));
+            return 1;
+        } else {
+            int days = 1;
+            while (!stack.isEmpty() && stack.peek().val <= price) {
+                days += stack.pop().days;
+            }
+            stack.push(new Node(price, days));
+            return days;
+        }
+    }
+
+    class Node {
+        int val;
+        int days;
+        Node(int val, int days) {
+            this.val = val;
+            this.days = days;
+        }
+    }
+}
